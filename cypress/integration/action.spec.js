5dea401f4bc6b9e18d22b7b559ebd215e20c3e2b
Dennis van den Berg <dennis.van.den.berg@dop.nu>
2020-03-09 15:53:00 +0100

    aaa

--- cypress/integration/action.spec.js
+++ cypress/integration/action.spec.js
@@ -2,7 +2,13 @@
 
 context('Actions', () => {
   beforeEach(() => {
-    cy.visit('https://example.cypress.io/commands/actions')
+    cy.visit('http://dennisvdberg.nl')
+  })
+
+  it('.click() - click on a DOM element', () => {
+    // https://on.cypress.io/click
+    cy.get('.nav-item.nav-link.projects').click()
+
   })
 
   // https://on.cypress.io/interacting-with-elements
@@ -65,58 +71,6 @@ context('Actions', () => {
       .find('[type="text"]').type('HALFOFF')
     cy.get('.action-form').submit()
       .next().should('contain', 'Your form has been submitted!')
-  })
-
-  it('.click() - click on a DOM element', () => {
-    // https://on.cypress.io/click
-    cy.get('.action-btn').click()
-
-    // You can click on 9 specific positions of an element:
-    //  -----------------------------------
-    // | topLeft        top       topRight |
-    // |                                   |
-    // |                                   |
-    // |                                   |
-    // | left          center        right |
-    // |                                   |
-    // |                                   |
-    // |                                   |
-    // | bottomLeft   bottom   bottomRight |
-    //  -----------------------------------
-
-    // clicking in the center of the element is the default
-    cy.get('#action-canvas').click()
-
-    cy.get('#action-canvas').click('topLeft')
-    cy.get('#action-canvas').click('top')
-    cy.get('#action-canvas').click('topRight')
-    cy.get('#action-canvas').click('left')
-    cy.get('#action-canvas').click('right')
-    cy.get('#action-canvas').click('bottomLeft')
-    cy.get('#action-canvas').click('bottom')
-    cy.get('#action-canvas').click('bottomRight')
-
-    // .click() accepts an x and y coordinate
-    // that controls where the click occurs :)
-
-    cy.get('#action-canvas')
-      .click(80, 75) // click 80px on x coord and 75px on y coord
-      .click(170, 75)
-      .click(80, 165)
-      .click(100, 185)
-      .click(125, 190)
-      .click(150, 185)
-      .click(170, 165)
-
-    // click multiple elements by passing multiple: true
-    cy.get('.action-labels>.label').click({
-      multiple: true
-    })
-
-    // Ignore error checking prior to clicking
-    cy.get('.action-opacity>.btn').click({
-      force: true
-    })
   })
 
 })
