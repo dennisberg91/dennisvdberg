4f49277b53b632800ced50877a59df1f104c9168
Dennis van den Berg <dennis.van.den.berg@dop.nu>
2020-03-09 15:57:38 +0100

    Click trigger

--- cypress/integration/action.spec.js
+++ cypress/integration/action.spec.js
@@ -5,5 +5,9 @@ context('Actions', () => {
     cy.visit('http://dennisvdberg.nl')
   })
 
+  it('.click() - click on a DOM element', () => {
+    // https://on.cypress.io/click
+    cy.get('.nav-item.nav-link.projects').click()
 
+  })
 })
