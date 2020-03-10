ba0bc6af56dc7b90a807310ba293b563915068f5
Dennis van den Berg <dennis.van.den.berg@dop.nu>
2020-03-10 10:44:37 +0100

    add tests

--- cypress/integration/search_spec.js
+++ cypress/integration/search_spec.js
@@ -1,6 +1,6 @@
 /// <reference types="Cypress" />
 
-context('Menu', () => {
+context('Search', () => {
   beforeEach(() => {
     cy.visit('http://dennisvdberg.nl');
   });
@@ -9,4 +9,12 @@ context('Menu', () => {
     // https://on.cypress.io/click
     cy.get('.nav-item.nav-link.projects').click();
   });
+
+  it('.type() - type into a DOM element', () => {
+    // https://on.cypress.io/type
+    cy.get('.form-control')
+      .type('Angular').should('have.value', 'Angular')
+      .clear()
+      .should('have.value', '')
+  })
 });
