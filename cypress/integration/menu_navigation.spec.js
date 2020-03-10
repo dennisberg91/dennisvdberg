5908af4d14bc73129efb342364bc518e3e3d013b
Dennis van den Berg <dennis.van.den.berg@dop.nu>
2020-03-10 08:14:38 +0100

    dd

---
+++ cypress/integration/menu_navigation.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+
+context('Actions', () => {
+  // beforeEach(() => {
+  //   cy.visit('http://dennisvdberg.nl')
+  // })
+
+  // it('.click() - click on a DOM element', () => {
+  //   // https://on.cypress.io/click
+  //   cy.get('.nav-item.nav-link.projects').click()
+  // })
+})
